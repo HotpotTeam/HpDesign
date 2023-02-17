992b4740e929ffd202121bd7786856f8bc475d3e
XuhuaYY <1551534592@qq.com>
2023-02-17 18:13:52 +0800

    完成input-number组件的丰富和完善

--- packages/input/input.vue
+++ packages/input/input.vue
@@ -220,7 +220,7 @@ export default {
     line-height: 40px;
     outline: none;
     padding: 0 15px;
-    margin: 2px 0;
+    margin: 0;
     transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
     width: 100%;
 
@@ -392,7 +392,7 @@ export default {
   border-radius: 4px;
   white-space: nowrap;
   width: max-content;
-  margin: 2px 0;
+  margin: 0;
   
 }
 
